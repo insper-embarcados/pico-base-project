1c573451346dea8d4bed2f267942368acc138542
ilacftemp <ilachafin@gmail.com>
2024-03-09 17:02:36 +0000

    jogo funcionando sem rgb

--- main/main.c
+++ main/main.c
@@ -7,11 +7,6 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-const int MOTOR_PIN_O = 10;
-const int MOTOR_PIN_Y = 11;
-const int MOTOR_PIN_P = 12;
-const int MOTOR_PIN_B = 13;
-
 const int LED_PIN_R = 18;
 const int LED_PIN_B = 19;
 const int LED_PIN_Y = 20;
@@ -21,15 +16,25 @@ const int BTN_PIN_Y = 2;
 const int BTN_PIN_R = 3;
 const int BTN_PIN_B = 4;
 const int BTN_PIN_G = 5;
-const int BTN_PIN_START = 28;
 
-const int BUZZER_PIN = 6;
+const int BTN_PIN_START = 14;
+
+const int BUZZER_PIN = 16;
+
+const int BTN_MODE_RED = 8;
+const int BTN_MODE_BLUE = 15;
 
 volatile bool btn_y_state = false;
 volatile bool btn_r_state = false;
 volatile bool btn_b_state = false;
 volatile bool btn_g_state = false;
+
 volatile bool start = false;
+
+volatile bool mode_black = false;
+
+volatile bool mode_red = false;
+volatile bool mode_blue = false;
 
 void btn_callback(uint gpio, uint32_t events) {
     if(gpio == BTN_PIN_R && events == GPIO_IRQ_EDGE_FALL){
@@ -42,35 +47,17 @@ void btn_callback(uint gpio, uint32_t events) {
         btn_g_state = true;
     } else if (gpio == BTN_PIN_START && events == GPIO_IRQ_EDGE_FALL){
         start = true;
-    }
-}
-
-void um_quarto_de_volta() {
-    for (int i = 0; i <= 512/4; i++){
-        gpio_put(MOTOR_PIN_P, 1);
-        sleep_ms(10);
-        gpio_put(MOTOR_PIN_P, 0);
-        gpio_put(MOTOR_PIN_B, 1);
-        sleep_ms(10);
-        gpio_put(MOTOR_PIN_B, 0);
-        gpio_put(MOTOR_PIN_O, 1);
-        sleep_ms(10);
-        gpio_put(MOTOR_PIN_O, 0);
-        gpio_put(MOTOR_PIN_Y, 1);
-        sleep_ms(10);
-        gpio_put(MOTOR_PIN_Y, 0);
+        mode_black = true;
+    } else if (gpio == BTN_MODE_RED && events == GPIO_IRQ_EDGE_FALL){
+        mode_red = true;
+        start = true;
+    } else if (gpio == BTN_MODE_BLUE && events == GPIO_IRQ_EDGE_FALL){
+        mode_blue = true;
+        start = true;
     }
 }
 
 void inicializa_dispositivos(){
-    gpio_init(MOTOR_PIN_P);
-    gpio_set_dir(MOTOR_PIN_P, GPIO_OUT);
-    gpio_init(MOTOR_PIN_B);
-    gpio_set_dir(MOTOR_PIN_B, GPIO_OUT);
-    gpio_init(MOTOR_PIN_O);
-    gpio_set_dir(MOTOR_PIN_O, GPIO_OUT);
-    gpio_init(MOTOR_PIN_Y);
-    gpio_set_dir(MOTOR_PIN_Y, GPIO_OUT);
 
     gpio_init(LED_PIN_B);
     gpio_set_dir(LED_PIN_B, GPIO_OUT);
@@ -101,6 +88,14 @@ void inicializa_dispositivos(){
     gpio_set_dir(BTN_PIN_START, GPIO_IN);
     gpio_pull_up(BTN_PIN_START);
 
+    gpio_init(BTN_MODE_RED);
+    gpio_set_dir(BTN_MODE_RED, GPIO_IN);
+    gpio_pull_up(BTN_MODE_RED);
+
+    gpio_init(BTN_MODE_BLUE);
+    gpio_set_dir(BTN_MODE_BLUE, GPIO_IN);
+    gpio_pull_up(BTN_MODE_BLUE);
+
     gpio_init(BUZZER_PIN);
     gpio_set_dir(BUZZER_PIN, GPIO_OUT);
 
@@ -109,6 +104,8 @@ void inicializa_dispositivos(){
     gpio_set_irq_enabled(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(BTN_PIN_START, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(BTN_MODE_RED, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(BTN_MODE_BLUE, GPIO_IRQ_EDGE_FALL, true);
 }
 
 void play_sound(int indice) {
@@ -162,11 +159,25 @@ int muda_sequencia(int sequencia[], int tamanho_sequencia){
 }
 
 void toca_sequencia(int sequencia[], int leds[], int tamanho_sequencia){
-    for(int i = 0; i < tamanho_sequencia; i++) {
-        gpio_put(leds[sequencia[i]], 1);
-        play_sound(sequencia[i]);
-        gpio_put(leds[sequencia[i]], 0);
-        sleep_ms(100);
+    if (mode_red) {
+        for(int i = 0; i < tamanho_sequencia; i++) {
+            gpio_put(leds[sequencia[i]], 1);
+            sleep_ms(100);
+            gpio_put(leds[sequencia[i]], 0);
+            sleep_ms(100);
+        }
+    } else if (mode_blue) {
+        for(int i = 0; i < tamanho_sequencia; i++) {
+            play_sound(sequencia[i]);
+            sleep_ms(100);
+        }
+    } else { // mode_black
+        for(int i = 0; i < tamanho_sequencia; i++) {
+            gpio_put(leds[sequencia[i]], 1);
+            play_sound(sequencia[i]);
+            gpio_put(leds[sequencia[i]], 0);
+            sleep_ms(100);
+        }
     }
 }
 
@@ -178,10 +189,38 @@ void wait_time(){
     srand((unsigned int)time_elapsed);
 }
 
+void play_fail_song() {
+    // Define the frequencies for the Mario "fail" song notes
+    #define E4 329
+    #define C4 261
+    #define G4 392
+    #define G3 196
+    #define E3 165
+    #define A3 220
+    #define B3 247
+    #define Bb3 233
+
+    int frequencies[] = {E4, E4, E4, C4, E4, G4, G3, C4, G3, E3, A3, B3, Bb3, A3};
+
+    int durations[] = {500, 200, 500, 300, 500, 500, 600, 500, 300, 500, 300, 300, 100, 500};
+    
+    for (int i = 0; i < sizeof(frequencies) / sizeof(frequencies[0]); i++) {
+        int t = 1000000 / (frequencies[i] * 2);
+        int n = durations[i] * 1000 / (t * 2);
+        for (int j = 0; j < n; j++) {
+            gpio_put(BUZZER_PIN, 1);
+            sleep_us(t);
+            gpio_put(BUZZER_PIN, 0);
+            sleep_us(t);
+        }
+        sleep_ms(50); // Pause between notes
+    }
+}
+
 int main(){
+
     stdio_init_all();
     inicializa_dispositivos();
-    printf("main\n");
 
     int tamanho_sequencia = 0;
     int sequencia[100] = {};
@@ -190,13 +229,25 @@ int main(){
 
     int leds[4] = {LED_PIN_B, LED_PIN_G, LED_PIN_R, LED_PIN_Y};
 
-    // bool acertou = true;
     bool perdeu = false;
 
     while(true){
         wait_time();
         if ((win_streak == tamanho_sequencia && !perdeu && start) || (perdeu && start)) {
             perdeu = false;
+
+            gpio_put(LED_PIN_B, 1);
+            gpio_put(LED_PIN_G, 1);
+            gpio_put(LED_PIN_R, 1);
+            gpio_put(LED_PIN_Y, 1);
+
+            sleep_ms(100);
+
+            gpio_put(LED_PIN_B, 0);
+            gpio_put(LED_PIN_G, 0);
+            gpio_put(LED_PIN_R, 0);
+            gpio_put(LED_PIN_Y, 0);
+
 
             sleep_ms(1000);
             tamanho_sequencia = muda_sequencia(sequencia, tamanho_sequencia);
@@ -207,7 +258,6 @@ int main(){
             for(int i = 0; i < tamanho_sequencia; i++){
                 
                 while (!btn_b_state && !btn_g_state && !btn_r_state && !btn_y_state);
-                printf("passou do while\n");
 
                 sleep_ms(100);
 
@@ -237,10 +287,12 @@ int main(){
                 win_streak += 1;
             }
             if (win_streak == tamanho_sequencia) {
-                um_quarto_de_volta();
                 play_right();
             } else {
                 start = false;
+                mode_black = false;
+                mode_red = false;
+                mode_blue = false;
 
                 gpio_put(LED_PIN_B, 1);
                 gpio_put(LED_PIN_G, 1);
@@ -248,12 +300,28 @@ int main(){
                 gpio_put(LED_PIN_Y, 1);
 
                 play_wrong();
-                win_streak = 0;
-                
+
                 gpio_put(LED_PIN_B, 0);
                 gpio_put(LED_PIN_G, 0);
                 gpio_put(LED_PIN_R, 0);
                 gpio_put(LED_PIN_Y, 0);
+
+                sleep_ms(100);
+
+                for (int i = 0; i < tamanho_sequencia-1; i++) { // conta a win streak
+                    gpio_put(LED_PIN_B, 1);
+                    gpio_put(LED_PIN_G, 1);
+                    gpio_put(LED_PIN_R, 1);
+                    gpio_put(LED_PIN_Y, 1);
+                    play_sound(4);
+                    gpio_put(LED_PIN_B, 0);
+                    gpio_put(LED_PIN_G, 0);
+                    gpio_put(LED_PIN_R, 0);
+                    gpio_put(LED_PIN_Y, 0);
+                    sleep_ms(100);
+                }
+
+                win_streak = 0;
     
                 memset(sequencia, 0, sizeof(sequencia));
                 tamanho_sequencia = 0;
